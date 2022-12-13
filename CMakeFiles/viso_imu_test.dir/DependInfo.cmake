8e35f95a049b6c5bf58d87840b82b515a5d0112c
This_boy_is_nobody <i_am_nobody@gmail.com>
2022-12-13 20:32:17 +0100

    Added cmake configuration

---
+++ CMakeFiles/viso_imu_test.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/aubin/viso_imu/src/ICM20948.cpp" "/home/aubin/viso_imu/CMakeFiles/viso_imu_test.dir/src/ICM20948.cpp.o"
+  "/home/aubin/viso_imu/src/detector.cpp" "/home/aubin/viso_imu/CMakeFiles/viso_imu_test.dir/src/detector.cpp.o"
+  "/home/aubin/viso_imu/src/imu.cpp" "/home/aubin/viso_imu/CMakeFiles/viso_imu_test.dir/src/imu.cpp.o"
+  "/home/aubin/viso_imu/src/main_unitary_test.cpp" "/home/aubin/viso_imu/CMakeFiles/viso_imu_test.dir/src/main_unitary_test.cpp.o"
+  "/home/aubin/viso_imu/src/matcher.cpp" "/home/aubin/viso_imu/CMakeFiles/viso_imu_test.dir/src/matcher.cpp.o"
+  "/home/aubin/viso_imu/src/transform_computer.cpp" "/home/aubin/viso_imu/CMakeFiles/viso_imu_test.dir/src/transform_computer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "include"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
