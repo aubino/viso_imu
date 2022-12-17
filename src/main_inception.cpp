23d734c4a394201939c409bc95fb61dd13069fd7
This_boy_is_nobody <i_am_nobody@gmail.com>
2022-12-17 03:37:07 +0100

    isdfbhzbf

--- src/main_inception.cpp
+++ src/main_inception.cpp
@@ -150,9 +150,10 @@ void viso_thread(std::string csi_pipeline, std::string config_file)
         img1.image = dis_buff;
         cv::undistort(img2.image,dis_buff,intrinsics,distorsion);
         img2.image = dis_buff;
+        std::pair<std::vector<cv::Point2f>,std::vector<cv::Point2f>> matches =  match_images(img2.image, img2.image,300,0.15,"Match_window","BruteForce-Hamming");
         //An then we compute the essential matrix
         cv::Mat R, t, pts,E;
-        pts = compute_transform_essential(img2.image,img1.image,intrinsics,R,t,E);
+        //pts = compute_transform_essential(img2.image,img1.image,intrinsics,R,t,E);
 
 
     }
