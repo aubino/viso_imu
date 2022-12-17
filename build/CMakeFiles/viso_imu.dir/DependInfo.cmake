d550bfa49f8aae264a210b94d8c6e5e2cbd8ce95
This_boy_is_nobody <i_am_nobody@gmail.com>
2022-12-17 16:48:48 +0100

    Added jetson hacks nano simple camera code for verification

--- build/CMakeFiles/viso_imu.dir/DependInfo.cmake
+++ build/CMakeFiles/viso_imu.dir/DependInfo.cmake
@@ -1,19 +1,28 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/aubin/viso_imu/src/ICM20948.cpp" "/home/aubin/viso_imu/build/CMakeFiles/viso_imu.dir/src/ICM20948.cpp.o"
+  "/home/aubin/viso_imu/src/detector.cpp" "/home/aubin/viso_imu/build/CMakeFiles/viso_imu.dir/src/detector.cpp.o"
+  "/home/aubin/viso_imu/src/imu.cpp" "/home/aubin/viso_imu/build/CMakeFiles/viso_imu.dir/src/imu.cpp.o"
+  "/home/aubin/viso_imu/src/main_inception.cpp" "/home/aubin/viso_imu/build/CMakeFiles/viso_imu.dir/src/main_inception.cpp.o"
+  "/home/aubin/viso_imu/src/matcher.cpp" "/home/aubin/viso_imu/build/CMakeFiles/viso_imu.dir/src/matcher.cpp.o"
+  "/home/aubin/viso_imu/src/transform_computer.cpp" "/home/aubin/viso_imu/build/CMakeFiles/viso_imu.dir/src/transform_computer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ERROR_CODE_HEADER_ONLY"
   )
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/aubin/viso_imu/src/ICM20948.cpp" "CMakeFiles/viso_imu.dir/src/ICM20948.cpp.o" "gcc" "CMakeFiles/viso_imu.dir/src/ICM20948.cpp.o.d"
-  "/home/aubin/viso_imu/src/detector.cpp" "CMakeFiles/viso_imu.dir/src/detector.cpp.o" "gcc" "CMakeFiles/viso_imu.dir/src/detector.cpp.o.d"
-  "/home/aubin/viso_imu/src/imu.cpp" "CMakeFiles/viso_imu.dir/src/imu.cpp.o" "gcc" "CMakeFiles/viso_imu.dir/src/imu.cpp.o.d"
-  "/home/aubin/viso_imu/src/main_inception.cpp" "CMakeFiles/viso_imu.dir/src/main_inception.cpp.o" "gcc" "CMakeFiles/viso_imu.dir/src/main_inception.cpp.o.d"
-  "/home/aubin/viso_imu/src/matcher.cpp" "CMakeFiles/viso_imu.dir/src/matcher.cpp.o" "gcc" "CMakeFiles/viso_imu.dir/src/matcher.cpp.o.d"
-  "/home/aubin/viso_imu/src/transform_computer.cpp" "CMakeFiles/viso_imu.dir/src/transform_computer.cpp.o" "gcc" "CMakeFiles/viso_imu.dir/src/transform_computer.cpp.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "../include"
+  "/usr/include/opencv4"
   )
 
 # Targets to which this target links.
