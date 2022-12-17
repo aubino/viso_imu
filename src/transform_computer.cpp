67dddedb5712c6a12ba0c5ab3299f873bf1ed24f
This_boy_is_nobody <i_am_nobody@gmail.com>
2022-12-17 21:59:06 +0100

    Same

--- src/transform_computer.cpp
+++ src/transform_computer.cpp
@@ -4,23 +4,27 @@ cv::Mat compute_transform_essential(const cv::Mat& img1, const cv::Mat& img2,cv:
 {
     auto points_pairs = filter_matches_with_disp(match_images(img1,img2,300,0.5f),cameraMatrix,5.0);
     //auto points_pairs = match_images(img1,img2,300,0.5f);
-
+    cv::Mat points;
+    cv::Mat inliers;
     if (points_pairs.first.size()<=7)
     {
         #ifdef DEBUG 
             std::cout<<"Not Enougth matches to compute transform between the two images"<<std::endl;
         #endif
+        return points;
     }
     else if(points_pairs.first.size()==7)
     {
         //to process 
+        return points;
     }
     else if(points_pairs.first.size()>7)
     {
         cv::Mat mask;
         Essential =  cv::findEssentialMat(points_pairs.first, points_pairs.second,cameraMatrix,cv::RANSAC,0.999, 1,mask);
-        cv::Mat points;
-        cv::Mat inliers;
+        #ifdef DEBUG 
+            std::cout<<"Essential materix is " << Essential << std::endl;
+        #endif
         cv::recoverPose(Essential,points_pairs.first,points_pairs.second,cameraMatrix,R,t,InfThresh, inliers,points);
         return points;
     }
